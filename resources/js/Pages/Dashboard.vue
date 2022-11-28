1b4577afaca819834209cc432a79424204bd9953
amitleuva1987 <amitleuva1987@gmail.com>
2022-11-28 11:12:09 +0530

    Make it in 1 bucket

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -15,72 +15,38 @@ import { Head } from "@inertiajs/inertia-vue3";
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <p class="text-center text-red-600">{{ error }}</p>
                 <div
                     class="grid grid-cols-3 overflow-hidden shadow-sm sm:rounded-lg space-x-5"
                 >
-                    <div>
-                        <h2 class="text-lg mb-2">Fruits</h2>
+                    <div v-for="bucket in data" :key="bucket.bucket">
+                        <h2 class="text-lg mb-2">{{ bucket.bucket }}</h2>
                         <draggable
-                            class="list-group"
-                            v-model="list1"
+                            :class="bucket.bucket"
+                            :list="bucket.values"
                             group="fruits"
                             itemKey="name"
+                            :move="checkElement"
                         >
                             <template #item="{ element }">
                                 <div
                                     class="list-group-item bg-white p-2 border-b-2 cursor-move"
                                     :class="
-                                        element.type == 'vegetable'
+                                        element.type == 'vegetable' &&
+                                        bucket.bucket == 'fruit'
                                             ? 'bg-red-500 text-white'
                                             : ''
                                     "
                                 >
                                     {{ element.name }}
-                                    <span v-if="element.type == 'vegetable'">
-                                        --> Placed in wrong bucket</span
+                                    <span
+                                        v-if="
+                                            element.type == 'vegetable' &&
+                                            bucket.bucket == 'fruit'
+                                        "
                                     >
-                                </div>
-                            </template>
-                        </draggable>
-                    </div>
-                    <div>
-                        <h2 class="text-lg mb-2">Vegetables</h2>
-                        <draggable
-                            class="list-group"
-                            :list="list2"
-                            group="fruits"
-                            itemKey="name"
-                        >
-                            <template #item="{ element }">
-                                <div
-                                    class="list-group-item bg-white p-2 border-b-2 cursor-move"
-                                    :class="
-                                        element.type == 'fruit'
-                                            ? 'bg-red-500 text-white'
-                                            : ''
-                                    "
-                                >
-                                    {{ element.name }}
-                                    <span v-if="element.type == 'fruit'">
-                                        --> Placed in wrong bucket</span
+                                        Placed in wrong bucket</span
                                     >
-                                </div>
-                            </template>
-                        </draggable>
-                    </div>
-                    <div>
-                        <h2 class="text-lg mb-2">Fruits & Vegetables</h2>
-                        <draggable
-                            class="list-group"
-                            :list="list3"
-                            group="fruits"
-                            itemKey="name"
-                        >
-                            <template #item="{ element }">
-                                <div
-                                    class="list-group-item bg-white p-2 border-b-2 cursor-move"
-                                >
-                                    {{ element.name }}
                                 </div>
                             </template>
                         </draggable>
@@ -99,27 +65,101 @@ export default {
     },
     data() {
         return {
-            list1: [
-                { name: "Apple", type: "fruit" },
-                { name: "Pinaple", type: "fruit" },
-                { name: "Orange", type: "fruit" },
-                { name: "Kiwi", type: "fruit" },
-                { name: "Mango", type: "fruit" },
-            ],
-            list2: [
-                { name: "Spinach", type: "vegetable" },
-                { name: "Okra", type: "vegetable" },
-                { name: "Potato", type: "vegetable" },
-            ],
-            list3: [
-                { name: "Banana", type: "fruit" },
-                { name: "Cauliflower", type: "vegetable" },
-                { name: "Grapes", type: "fruit" },
-                { name: "Garlic", type: "vegetable" },
-                { name: "Brocolli", type: "vegetable" },
-                { name: "Tomato", type: "vegetable" },
+            error: "",
+            data: [
+                {
+                    bucket: "fruit",
+                    values: [
+                        {
+                            id: 1,
+                            name: "Apple",
+                            type: "fruit",
+                            bucket: "fruit",
+                        },
+                        {
+                            id: 2,
+                            name: "Pinaple",
+                            type: "fruit",
+                            bucket: "fruit",
+                        },
+                        {
+                            id: 3,
+                            name: "Orange",
+                            type: "fruit",
+                            bucket: "fruit",
+                        },
+                        { id: 4, name: "Kiwi", type: "fruit", bucket: "fruit" },
+                        {
+                            id: 5,
+                            name: "Mango",
+                            type: "fruit",
+                            bucket: "fruit",
+                        },
+                    ],
+                },
+                {
+                    bucket: "vegetable",
+                    values: [
+                        {
+                            id: 6,
+                            name: "Spinach",
+                            type: "vegetable",
+                            bucket: "vegetable",
+                        },
+                        {
+                            id: 7,
+                            name: "Okra",
+                            type: "vegetable",
+                            bucket: "vegetable",
+                        },
+                        {
+                            id: 8,
+                            name: "Potato",
+                            type: "vegetable",
+                            bucket: "vegetable",
+                        },
+                    ],
+                },
+                {
+                    bucket: "common",
+                    values: [
+                        { name: "Banana", type: "fruit" },
+                        { name: "Cauliflower", type: "vegetable" },
+                        { name: "Grapes", type: "fruit" },
+                        { name: "Garlic", type: "vegetable" },
+                        { name: "Brocolli", type: "vegetable" },
+                        { name: "Tomato", type: "vegetable" },
+                    ],
+                },
             ],
         };
+    },
+    methods: {
+        checkElement: function (evt) {
+            if (
+                evt.to.classList.contains("vegetable") == true &&
+                evt.draggedContext.element.type == "fruit"
+            ) {
+                this.error = "You choose wrong bucket";
+                setTimeout(() => (this.error = ""), 3000);
+                return false;
+            }
+
+            if (
+                evt.to.classList.contains("fruit-group") == true &&
+                evt.draggedContext.element.type == "vegetable"
+            ) {
+                this.error = "You choose wrong bucket";
+                setTimeout(() => (this.error = ""), 3000);
+                return false;
+            }
+
+            return true;
+            // console.log(evt.moved.element);
+        },
+        checkNewElement: function (item) {
+            // console.log(item);
+        },
     },
 };
 </script>
